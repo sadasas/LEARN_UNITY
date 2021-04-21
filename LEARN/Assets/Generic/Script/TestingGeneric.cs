aa2d00d645bcc9b6217a0a9b544d9b52561de3e9
sadasas <setio101199@gmail.com>
2021-04-22 05:26:00 +0700

    v1

--- LEARN/Assets/Generic/Script/TestingGeneric.cs
+++ LEARN/Assets/Generic/Script/TestingGeneric.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,16 +33,75 @@ public class TestingGeneric : MonoBehaviour
 
     private void Start()
     {
+        //call and set
         int[] intArray = CreateArray(1, 2);
         Debug.Log(intArray.Length + " " + intArray[0] + " " + intArray[1]);
 
         string[] stringArray = CreateArray("aa", "bb");
         Debug.Log(stringArray.Length + " " + stringArray[0] + " " + stringArray[1]);
+
+        MultipleGeneric<string, int>("wdwd", 2);
+
+        myClass<int> myc = new myClass<int>();
+        myc.value = 100;
+        Debug.Log(myc.value);
+        myClass<string> mcy = new myClass<string>();
+        mcy.value = "wahyu";
+        Debug.Log(mcy.value);
+
+        myclass<EnemyMinion> EM = new myclass<EnemyMinion>(new EnemyMinion());
     }
 
+    //single generic
     private T[] CreateArray<T>(T firstArgument, T secondArgument)
     {
         return new T[] { firstArgument, secondArgument };
+    }
+
+    //multiple generic
+    private void MultipleGeneric<T1, T2>(T1 first, T2 Second)
+    {
+        Debug.Log(first);
+        Debug.Log(Second);
+    }
+
+    //class generic
+    public class myClass<T>
+    {
+        public T value;
+    }
+
+    //class generic
+
+    public class myclass<T> where T : Enemy<int>
+    {
+        public T value;
+
+        public myclass(T value)
+        {
+            value.Damage(100);
+        }
+    }
+
+    public interface Enemy<T>
+    {
+        void Damage(T t);
+    }
+
+    public class EnemyMinion : Enemy<int>
+    {
+        public void Damage(int i)
+        {
+            Debug.Log("EnemyMinion.Damage" + i);
+        }
+    }
+
+    public class EnemyArcher : Enemy<int>
+    {
+        public void Damage(int i)
+        {
+            Debug.Log("EnemyArcher.Damage" + i);
+        }
     }
 
     #endregion using Generic
