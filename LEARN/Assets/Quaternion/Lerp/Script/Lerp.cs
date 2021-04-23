00f414a057d484f61f7996a722ab166da636d3cc
sadasas <setio101199@gmail.com>
2021-04-23 16:18:11 +0700

    Lerp
    basic

---
+++ LEARN/Assets/Quaternion/Lerp/Script/Lerp.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp : MonoBehaviour
+{
+    //Interpolates between a and b by t and normalizes the result afterwards. The parameter t is clamped to the range [0, 1].
+    //This is faster than Slerp but looks worse if the rotations are far apart.
+
+    //public static Quaternion Lerp(Quaternion a, Quaternion b, float t);
+
+    public Transform from;
+
+    public Transform to;
+
+    private float speed = 0.1f;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, speed * Time.time);
+    }
+}
