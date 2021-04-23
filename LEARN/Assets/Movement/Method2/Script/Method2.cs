79f25c308a9e418f7d32a3069351f122694a7d8d
sadasas <setio101199@gmail.com>
2021-04-24 06:14:11 +0700

    Method2
    With transform.transformDirection
    but cannot rotate self because there are collision  transform.position and transform.rotation

--- LEARN/Assets/Movement/Method2/Script/Method2.cs
+++ LEARN/Assets/Movement/Method2/Script/Method2.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Method2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    private Vector3 movement;
+    public float rotationSpeed;
+
+    private Vector3 currentPosition;
+
+    private void Update()
     {
-        
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement.Normalize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        transform.position += transform.TransformDirection(movement * Speed * Time.deltaTime);
+        // currentPosition = transform.position;
+        if (movement.magnitude > 0.1)
+        {
+            //problem with transform rotation
+
+            // transform.forward = movement;
+
+            /*float y = movement.x + movement.z;
+            Vector3 lookforward = new Vector3(0, y, 0);
+            transform.Rotate(lookforward, Space.Self);*/
+
+            //transform.rotation = Quaternion.LookRotation(movement);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+
+            /*   Quaternion lookForward = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(movement), rotationSpeed);
+               transform.Rotate(lookForward.eulerAngles, Space.World);*/
+
+            //transform.LookAt(transform.forward);
+        }
     }
 }
