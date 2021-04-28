6753bcedb690a361c8052416ef6c5a386e12be19
sadasas <setio101199@gmail.com>
2021-04-28 22:29:07 +0700

    Raycast
    basic

--- LEARN/Assets/Physics/Raycast/Script/Raycast.cs
+++ LEARN/Assets/Physics/Raycast/Script/Raycast.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //ublic static bool Raycast(Vector3 origin, Vector3 direction, float maxDistance = Mathf.Infinity, int layerMask = DefaultRaycastLayers, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.UseGlobal);
+    //bool Returns true if the ray intersects with a Collider, otherwise false.
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate()
+    {// This would cast rays only against colliders in layer 1
+        int layerMask = 1 << 1;
+
+        // Does the ray intersect any objects which are in the player layer.
+        if (Physics.Raycast(transform.position, transform.forward, Mathf.Infinity, layerMask))
+        {
+            Debug.Log("The ray hit the player");
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+        }
     }
 }
