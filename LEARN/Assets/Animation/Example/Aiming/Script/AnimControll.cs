d506f112180b02962ae5a624b1815bde3d11446c
wahyusrp <setio101199@gmail.com>
2021-08-09 14:19:24 +0700

    .

--- LEARN/Assets/Animation/Example/Aiming/Script/AnimControll.cs
+++ LEARN/Assets/Animation/Example/Aiming/Script/AnimControll.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class AnimControll : MonoBehaviour
 {
     Animator animator;
-
+    public Rig aiming;
+    public float aimDuration = 0.3f;
+    
 
     private void Start()
     {
@@ -16,7 +19,18 @@ public class AnimControll : MonoBehaviour
     {
         bool aimPress = Input.GetKey(KeyCode.E);
 
-        if (aimPress) animator.SetBool("IsAiming", true);
-        else { animator.SetBool("IsAiming", false); };
+        if (aimPress)
+        {
+            animator.SetBool("IsAiming", true);
+            aiming.weight += Time.deltaTime / aimDuration;
+
+
+        }
+        else
+        { 
+            animator.SetBool("IsAiming", false);
+            aiming.weight -= Time.deltaTime / aimDuration;
+
+        };
     }
 }
